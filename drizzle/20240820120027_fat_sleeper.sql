753bc9a8f3f9b416b6cd22c73faa503f11f51fa4
krypton-0x00 <shakirgulzar0x00gmail.com>
2024-08-20 22:14:50 +0530

    fix:route errors in next

--- drizzle/20240820120027_fat_sleeper.sql
+++ drizzle/20240820120027_fat_sleeper.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS "feedbacks" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"project_id" varchar,
+	"project_id" integer,
 	"user_name" text,
 	"user_email" text,
-	"message" text
+	"message" text,
+	"rating" integer
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "projects" (
@@ -12,4 +13,12 @@ CREATE TABLE IF NOT EXISTS "projects" (
 	"description" text,
 	"url" text,
 	"user_id" varchar
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "subscriptions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" varchar,
+	"stripe_customer_id" text,
+	"stripe_subscription_id" text,
+	"subscribed" boolean
 );
