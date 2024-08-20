ecc5e4d4acca7cb617e1564967c9151c7272347c
krypton-0x00 <shakirgulzar0x00gmail.com>
2024-08-21 01:47:36 +0530

    fix:Some UI changes

--- app/(user)/dashboard/projects-list.tsx
+++ app/(user)/dashboard/projects-list.tsx
@@ -1,6 +1,13 @@
 import { InferSelectModel } from "drizzle-orm";
 import { projects } from "@/db/schema";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import SubscribeBtn from "../payments/subscribe-btn";
@@ -16,7 +23,6 @@ type Props = {
 };
 
 const ProjectsList = (props: Props) => {
-
   return (
     <div>
       <ul className="grid grid-cols-1 md:grid-cols-3 m-5 p-4 gap-4">
@@ -35,20 +41,25 @@ const ProjectsList = (props: Props) => {
             </Card>
           </li>
         ))}
-        {props.subscribed !== true && props.projects?.length > maxFreeProjects ?
+        {props.subscribed !== true &&
+        props.projects?.length > maxFreeProjects ? (
           <Card className="max-w-[350px] flex flex-col h-full bg-gray-300">
             <CardHeader className="flex-1">
               <CardTitle className="flex flex-row text-sm md:text-lg items-center">
                 <Lock className="h-4 w-4 md:h-8 md:w-8 mr-2" />
-                <span>Upgrade to Premium</span></CardTitle>
-              <CardDescription className="mt-3">Unlock unlimited projects</CardDescription>
+                <span>Upgrade to Premium</span>
+              </CardTitle>
+              <CardDescription className="mt-3">
+                Unlock unlimited projects
+              </CardDescription>
             </CardHeader>
             <div className="w-fit mx-auto mb-4">
               <SubscribeBtn price={monthlyPlanId} />
             </div>
-          </Card> : null}
+          </Card>
+        ) : null}
       </ul>
     </div>
-  )
-}
+  );
+};
 export default ProjectsList;
--- components/page-header.tsx
+++ components/page-header.tsx
@@ -12,7 +12,7 @@ import HeaderMenu from "@/components/header-menu";
 const PageHeader = () => {
   return (
     <header className="sticky inset-x-0 top-0 z-30 w-full transition-all bg-white/5 backdrop-blur-md">
-      <div className="w-full max-w-screen-xl px-2.5 lg:px-20 relative mx-auto border-b">
+      <div className="w-full max-w-screen-xl px-2.5 lg:px-20 relative mx-auto ">
         <div className="flex h-14 items-center justify-between">
           <h1 className="text-2xl font-bold">Go Feed</h1>
           <div>
