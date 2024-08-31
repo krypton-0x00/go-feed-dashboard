93dc360c95a2f3458ee15151d21bd122c9be4f63
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-09-01 01:02:35 +0530

    Fixed:Bug during payments

---
+++ app/(user)/payments/checkout-success/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-3xl flex justify-center items-center">
+      <h1>Payment Sucessfull</h1>
+    </div>
+  );
+};
+
+export default page;
