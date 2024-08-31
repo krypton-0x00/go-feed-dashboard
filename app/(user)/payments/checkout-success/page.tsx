19e4882898ab8e667e7629386aa3318aa3ea926e
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-09-01 01:11:59 +0530

    Fixed:Added redirect btn

--- app/(user)/payments/checkout-success/page.tsx
+++ app/(user)/payments/checkout-success/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
     <div className="text-3xl flex justify-center items-center">
       <h1>Payment Sucessfull</h1>
+      <Button>
+        <Link href="/dashboard">Back to Dashboard</Link>
+      </Button>
     </div>
   );
 };
